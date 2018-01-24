2030c4cf1a31d13c9293dc54a16df5a9dc038e59
Wil Gieseler <wil@wilgieseler.com>
2018-01-24 15:01:18 -0800

    test stuff

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "lookml-test", git: "https://github.com/looker/lookml-test-runner.git"
+gem "minitest"
---
+++ test.rb
@@ -0,0 +1,30 @@
+require "bundler/setup"
+require "minitest/autorun"
+require 'lookml/test'
+
+class TestMeme < Minitest::Test
+  def setup
+    sdk = LookerSDK::Client.new(
+      client_id: ENV['LOOKER_TEST_RUNNER_CLIENT_ID'],
+      client_secret: ENV['LOOKER_TEST_RUNNER_CLIENT_SECRET'],
+      api_endpoint: ENV['LOOKER_TEST_RUNNER_ENDPOINT'],
+    )
+    @runner = LookMLTest::Runner.new(
+      sdk: sdk,
+      branch: ENV['TRAVIS_BRANCH'],
+      email: `git log -1 --pretty=format:'%ae'`.strip,
+      remote_url: `git config --get remote.origin.url`.strip,
+    )
+  end
+
+  def test_basic
+    result = @runner.sdk.run_inline_query("json_detail", {
+      model: "lookml_test_test_fun",
+      view: "users",
+      fields: ["users.id"],
+      sorts: ["users.id asc"],
+      limit: 1,
+    })
+    assert_equal result.data[0]["users.id"].value 1
+  end
+end
